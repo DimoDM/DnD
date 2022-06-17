80147c4199d348bb3c51665da062ce735f9a8f86
DimoDM <dimo_marino@abv.bg>
2022-06-18 02:30:05 +0300

    commit of before

--- ECS/MonsterAIComponent.h
+++ ECS/MonsterAIComponent.h
@@ -4,12 +4,34 @@
 #include"../Game.h"
 #include"Components.h"
 #include<iostream>
+#include"../Structures/Vector2D.h"
+#include<math.h>
 using namespace std;
 
 class MonsterAIComponent : public Component
 {
 	bool isVisible = true;
-	Collection<Entity>* players = nullptr;
+	Collection<Entity>* players;
+	TransformComponent* transform;
+
+	const int getIndexOfTheClosestPlayer() const {
+		Vector2D playerPos;
+		const Vector2D MonsterPos = transform->getPos();
+		Vector2D distance2D;
+		int distance = ConsoleViewer::getInstance()->getMaxWidth() * 3;
+		int index;
+		for (int i = 0; i < players->getSize(); i++) {
+			playerPos = players->getElementByIndex(0)->getComponent<TransformComponent>().getPos();
+			distance2D = (playerPos.Subtract(MonsterPos));
+			int curDis = abs(distance2D.x) + abs(distance2D.y);
+			if (distance > curDis) {
+				distance = curDis;
+				index = i;
+			}
+		}
+	}
+
+
 
 public:
 
@@ -24,9 +46,12 @@ public:
 
 
 	void init() override {
+		if (entity->hasComponent<TransformComponent>())
+		transform = &entity->getComponent<TransformComponent>();
 	}
 
 	void update() override {
+
 
 	}
 	void draw() override {
