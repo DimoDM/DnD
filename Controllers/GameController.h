3bede2e27832bba4ed6fa7c9e22725c9ef3d07a9
DimoDM <dimo_marino@abv.bg>
2022-06-19 15:11:03 +0300

    start game operation started2

--- Controllers/GameController.h
+++ Controllers/GameController.h
@@ -1,1 +1,22 @@
 #pragma once
+#include"../ConsoleViewer.h"
+#include"../Menu.h"
+
+class GameController {
+
+
+
+	const char* fileDir = "../data";
+	const char* raceOptions[3] = { "Human", "Mage", "Warrior" };
+	const Menu* raceMenu = new Menu("SelectRace: ", raceOptions, 3, ConsoleViewer::getInstance());
+
+	ConsoleViewer* viewer = nullptr;
+
+	void loadPlayerFromFile();
+
+
+public:
+	GameController(ConsoleViewer* consoleViewer);
+
+	void createPlayer();
+};
