1d51441d46b4c5671cad14cd123bc023a869f12b
DimoDM <dimo_marino@abv.bg>
2022-06-27 19:58:17 +0300

    master version

--- main.cpp
+++ main.cpp
@@ -1,5 +1,3 @@
-#include<iostream>
-#include"Game.h"
 #include<Windows.h>
 #include<conio.h>
 #include<time.h>
@@ -9,19 +7,4 @@ int main()
 {
 	Engine e;
 		e.run();
-	//system("mode 650");
-	//SetConsoleDisplayMode(GetStdHandle(STD_OUTPUT_HANDLE), CONSOLE_FULLSCREEN_MODE, 0);
-	//system("mode con COLS=700");
-	//ShowWindow(GetConsoleWindow(), SW_MAXIMIZE);
-	////SendMessage(GetConsoleWindow(), WM_SYSKEYDOWN, VK_RETURN, 0x20000000);
-	//system("Title = DnD");
-	//Game game;
-	//game.init();
-	//while (true) {
-	//	system("CLS");
-	//	game.draw();
-	//	game.update();
-	//}
-	//std::cout << "she-wolf";
-	//return 0;
 }
